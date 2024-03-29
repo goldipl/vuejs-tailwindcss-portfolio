45c9ce17e08fde058148d4c86d2a2851e8ad4a48
goldipl <mgodlewski92@gmail.com>
2024-03-29 09:45:52 +0100

    seperate Header to a new component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import HeaderComponent from './components/common/HeaderComponent.vue'
 </script>
 
 <template>
-  <header
-    class="w-full before:content-[''] before:block before:bg-gradient-to-t from-purple-600 from-15% to-black before:top-0 before:left-0 before:absolute before:h-16 before:w-full mb-4"
-  >
-    <div
-      class="relative bg-gradient-to-t from-purple-600 from-15% to-black flex justify-between items-center h-16"
-    >
-      <RouterLink to="/">
-        <p class="relative text-white font-rock text-2xl top-[-4px]">
-          Marcin Godlewski
-          <span class="absolute bottom-[-16px] right-[-16px] text-sm">Web Developer</span>
-        </p>
-      </RouterLink>
-      <nav class="flex justify-between items-center w-3/5">
-        <RouterLink class="text-gray-300 text-lg hover:text-white" to="/">Home</RouterLink>
-        <RouterLink class="text-gray-300 text-lg hover:text-white" to="/about">About</RouterLink>
-        <RouterLink class="text-gray-300 text-lg hover:text-white" to="/portfolio"
-          >Portfolio</RouterLink
-        >
-        <RouterLink class="text-gray-300 text-lg hover:text-white" to="/references"
-          >References</RouterLink
-        >
-        <RouterLink class="text-gray-300 text-lg hover:text-white" to="/contact"
-          >Contact</RouterLink
-        >
-      </nav>
-    </div>
-  </header>
+  <HeaderComponent />
   <RouterView />
 </template>
 
