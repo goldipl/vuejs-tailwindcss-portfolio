130f905982a828c245890061e166604d4d6d3701
goldipl <mgodlewski92@gmail.com>
2024-03-29 09:06:36 +0100

    add a new font family theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
+    fontFamily: {
+      'rock': [ 'Rock Salt', 'cursive'],
+    },
     extend: {},
   },
   plugins: [],
