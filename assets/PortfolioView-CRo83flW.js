3948f443efe2f4c8edf94863a7e3b285ea36ccc3
goldipl <mgodlewski92@gmail.com>
2024-03-29 13:08:27 +0100

    adding dist

--- assets/PortfolioView-CRo83flW.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as e,c as s,a as t}from"./index-CSGKJzMf.js";const c={},a={class:"wrapper portfolio-page"},r=t("h1",null,"This is a portfolio page",-1),_=[r];function n(i,l){return e(),s("div",a,_)}const f=o(c,[["render",n]]);export{f as default};
