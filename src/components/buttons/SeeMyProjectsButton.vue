5001016474c712c9b2ba4fca7f8b096760db47b7
goldipl <mgodlewski92@gmail.com>
2024-04-02 13:10:48 +0200

    delete right margin

--- src/components/buttons/SeeMyProjectsButton.vue
+++ src/components/buttons/SeeMyProjectsButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center items-center my-4">
     <a
-      class="text-gray-300 px-8 py-3 me-4 border border-purple-600 hover:text-purple-600 hover:bg-white transition-all duration-500"
+      class="text-gray-300 px-8 py-3 border border-purple-600 hover:text-purple-600 hover:bg-white transition-all duration-500"
       href="#about-me-section"
       >Zobacz więcej moich projektów</a
     >
