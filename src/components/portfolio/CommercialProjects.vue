979d22c21b85841caf58db9939c9580561aec9fe
goldipl <mgodlewski92@gmail.com>
2024-04-02 21:29:58 +0200

    add commercial projects

---
+++ src/components/portfolio/CommercialProjects.vue
@@ -0,0 +1,125 @@
+<template>
+  <section class="my-20">
+    <h2 class="text-4xl mb-4 text-center">Projekty komercyjne</h2>
+    <p class="mb-2 text-center">
+      Wybrane projekty stron AWD realizowane w ramach pracy jako Web Developer w Grupie ZPR Media
+      S.A.
+    </p>
+    <div class="flex flex-wrap justify-center items-center gap-5 my-6">
+      <!-- Project -->
+      <div class="relative my-4 border border-1 border-purple-600">
+        <img
+          src="./../../assets/img/portfolio/commercial_zpr/CommercialWebsite01.jpg"
+          alt="se.pl"
+          width="460"
+          height="230"
+          class="h-auto"
+        />
+        <div
+          class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+        >
+          <a
+            class="uppercase font-semibold text-white text-sm hover:text-black"
+            href="https://www.se.pl/"
+            >live</a
+          >
+        </div>
+      </div>
+      <!-- Project -->
+      <div class="relative my-4 border border-1 border-purple-600">
+        <img
+          src="./../../assets/img/portfolio/commercial_zpr/CommercialWebsite02.jpg"
+          alt="poradnikzdrowie.pl"
+          width="460"
+          height="230"
+          class="h-auto"
+        />
+        <div
+          class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+        >
+          <a
+            class="uppercase font-semibold text-white text-sm hover:text-black"
+            href="https://www.poradnikzdrowie.pl/"
+            >live</a
+          >
+        </div>
+      </div>
+      <!-- Project -->
+      <div class="relative my-4 border border-1 border-purple-600">
+        <img
+          src="./../../assets/img/portfolio/commercial_zpr/CommercialWebsite03.jpg"
+          alt="beszamel.se.pl"
+          width="460"
+          height="230"
+          class="h-auto"
+        />
+        <div
+          class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+        >
+          <a
+            class="uppercase font-semibold text-white text-sm hover:text-black"
+            href="https://beszamel.se.pl/"
+            >live</a
+          >
+        </div>
+      </div>
+      <!-- Project -->
+      <div class="relative my-4 border border-1 border-purple-600">
+        <img
+          src="./../../assets/img/portfolio/commercial_zpr/CommercialWebsite04.jpg"
+          alt="mediateka.pl"
+          width="460"
+          height="230"
+          class="h-auto"
+        />
+        <div
+          class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+        >
+          <a
+            class="uppercase font-semibold text-white text-sm hover:text-black"
+            href="https://mediateka.pl/"
+            >live</a
+          >
+        </div>
+      </div>
+      <!-- Project -->
+      <div class="relative my-4 border border-1 border-purple-600">
+        <img
+          src="./../../assets/img/portfolio/commercial_zpr/CommercialWebsite05.jpg"
+          alt="eska.pl"
+          width="460"
+          height="230"
+          class="h-auto"
+        />
+        <div
+          class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+        >
+          <a
+            class="uppercase font-semibold text-white text-sm hover:text-black"
+            href="https://www.eska.pl/"
+            >live</a
+          >
+        </div>
+      </div>
+      <!-- Project -->
+      <div class="relative my-4 border border-1 border-purple-600">
+        <img
+          src="./../../assets/img/portfolio/commercial_zpr/CommercialWebsite06.jpg"
+          alt="muratordom.pl/"
+          width="460"
+          height="230"
+          class="h-auto"
+        />
+        <div
+          class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+        >
+          <a
+            class="uppercase font-semibold text-white text-sm hover:text-black"
+            href="https://muratordom.pl/"
+            >live</a
+          >
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import ShortHeroComponent from '../components/portfolio/ShortHeroComponent.vue'
+import CommercialProjects from '../components/portfolio/CommercialProjects.vue'
 </script>
 
 <template>
   <ShortHeroComponent />
-  <div class="wrapper portfolio-page">
-    <h1>This is a portfolio page</h1>
-  </div>
+  <CommercialProjects />
 </template>
 
 <style scoped></style>
