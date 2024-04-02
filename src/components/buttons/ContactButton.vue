c0a7c91d27ade56c0e16e8d40340963c81707e45
goldipl <mgodlewski92@gmail.com>
2024-04-02 13:10:57 +0200

    add contact button with styles

---
+++ src/components/buttons/ContactButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="flex justify-center items-center my-6">
+    <RouterLink
+      class="text-black px-8 py-3 border border-purple-600 hover:text-white hover:bg-purple-600 hover:border-black transition-all duration-500"
+      to="/contact"
+      >Skontaktuj się ze mną</RouterLink
+    >
+  </div>
+</template>
