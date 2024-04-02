90dd3e6268ae7a01f07962fd2cb46b20ae500385
goldipl <mgodlewski92@gmail.com>
2024-04-02 13:21:38 +0200

    add a new portfolio button

---
+++ src/components/buttons/PortfolioButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="flex justify-center items-center my-6">
+    <RouterLink
+      class="text-white px-8 py-3 border border-purple-600 hover:text-purple-600 hover:border-white hover:bg-white transition-all duration-500"
+      to="/portfolio"
+      >Zobacz moje projekty</RouterLink
+    >
+  </div>
+</template>
