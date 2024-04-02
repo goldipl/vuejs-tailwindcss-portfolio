5474ce0d36f90ff3a496d9196582fdd9167ce806
goldipl <mgodlewski92@gmail.com>
2024-04-02 10:24:17 +0200

    add skills & tools sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,16 @@
 import HeroComponent from '../components/home/HeroComponent.vue'
 import AboutMeSection from '../components/home/AboutMeSection.vue'
 import ServicesSection from '../components/home/ServicesSection.vue'
+import SkillsSection from '../components/home/SkillsSection.vue'
+import ToolsSection from '../components/home/ToolsSection.vue'
 </script>
 
 <template>
   <HeroComponent />
   <AboutMeSection />
   <ServicesSection />
+  <SkillsSection />
+  <ToolsSection />
 </template>
 
 <style>
