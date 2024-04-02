418bdda3cf3dac3053bafe64221414ef7e70cf9a
goldipl <mgodlewski92@gmail.com>
2024-04-02 16:52:55 +0200

    add contact data & map sections

---
+++ src/components/contact/ContactDataSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <section id="contact-map-section">
+    <div class="wrapper my-24">
+      <h2 class="text-2xl my-2 text-left">Kontakt mailowy:</h2>
+      <p class="text-left text-lg">
+        E-mail:
+        <a
+          class="ms-2 text-purple-600 hover:underline text-base"
+          href="mailto:mgodlewski92@gmail.com"
+          >mgodlewski92@gmail.com</a
+        >
+      </p>
+      <h2 class="text-2xl mt-8 mb-2 text-left">Social media:</h2>
+      <p class="text-left text-lg">
+        GitHub:
+        <a class="ms-2 text-purple-600 hover:underline text-base" href="https://github.com/goldipl"
+          >https://github.com/goldipl</a
+        >
+      </p>
+      <p class="text-left text-lg">
+        Linkedin:
+        <a
+          class="ms-2 text-purple-600 hover:underline text-base"
+          href="https://www.linkedin.com/in/marcin-godlewski-153897139/"
+          >https://www.linkedin.com/in/marcin-godlewski-153897139/</a
+        >
+      </p>
+    </div>
+  </section>
+</template>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import ShortHeroComponent from '../components/contact/ShortHeroComponent.vue'
+import ContactMapSection from '../components/contact/ContactMapSection.vue'
+import ContactDataSection from '../components/contact/ContactDataSection.vue'
 </script>
 
 <template>
   <ShortHeroComponent />
-  <div class="wrapper contact-page">
-    <h1>This is a contact page</h1>
-  </div>
+  <ContactDataSection />
+  <ContactMapSection />
 </template>
-
-<style scoped></style>
