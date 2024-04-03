59ea0750a8af0da5421f64049dba16decc67ede4
goldipl <mgodlewski92@gmail.com>
2024-04-03 10:51:38 +0200

    add static website projects to portfolio

---
+++ src/components/portfolio/StaticWebsitesProjects.vue
@@ -0,0 +1,347 @@
+<template>
+  <section class="my-20">
+    <div class="projects-container mb-24">
+      <h2 class="text-4xl mb-4 text-center">Projekty stron statycznych</h2>
+      <p class="text-center">
+        Projekty kodowane zgodnie z technologią <strong>RWD</strong>, w głównej mierze oparte na
+        stacku: <strong>HTML5, CSS3, SCSS, Vanilla JavaScript</strong>.
+      </p>
+      <p class="text-center">
+        Rotatory użyte w projektach korzystają z biblioteki
+        <strong>Swiper.js</strong>.
+      </p>
+      <div class="wide-wrapper flex flex-wrap justify-center items-center gap-4 my-6">
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_01.jpg"
+            alt="AgricultureVideoWebpage"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/AgricultureVideoWebpage/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/AgricultureVideoWebpage"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_02.jpg"
+            alt="Cooking-with-passion"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/Cooking-with-passion/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/Cooking-with-passion"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_03.jpg"
+            alt="Dentist-WebPage"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/Dentist-WebPage/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/Dentist-WebPage"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_04.jpg"
+            alt="fizjomedica.radom.pl"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://www.fizjomedica.radom.pl/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/MedicalCenterWebPage"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_05.jpg"
+            alt="FoodBookKaroli"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/FoodBookKaroli/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/FoodBookKaroli"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_06.jpg"
+            alt="Landing Page Template"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/WebPage-Template/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/WebPage-Template"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_07.jpg"
+            alt="Bądź Wsparciem"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/BadzWsparciemWebPage/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/BadzWsparciemWebPage"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_08.jpg"
+            alt="Railway_Webpage"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/Railway_Webpage/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/Railway_Webpage"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_09.jpg"
+            alt="metroprojekt.pl"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://www.metroprojekt.pl/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/Railway_Website_new"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_10.jpg"
+            alt="Games-WebPage"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/Games-WebPage/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/Games-WebPage"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_11.jpg"
+            alt="sigmont_webpage"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/sigmont_webpage/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/sigmont_webpage"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_12.jpg"
+            alt="Jezenie Karoli, Bezglutenowa Karola"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/jedzenie-karoli/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/jedzenie-karoli"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.projects-container.murator-projekty {
+  position: relative;
+  height: auto;
+  background-image: linear-gradient(
+      to top,
+      rgba(0, 255, 247, 0.1294117647) -125%,
+      rgba(0, 0, 0, 0.5294117647) 95%
+    ),
+    url(./../../assets/img/hero/hero.jpg);
+  color: #ffffff;
+  background-position: bottom;
+  background-repeat: no-repeat;
+  background-size: 600vh;
+  background-attachment: fixed;
+}
+</style>
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import ShortHeroComponent from '../components/portfolio/ShortHeroComponent.vue'
 import CommercialProjects from '../components/portfolio/CommercialProjects.vue'
+import StaticWebsitesProjects from '../components/portfolio/StaticWebsitesProjects.vue'
 </script>
 
 <template>
   <ShortHeroComponent />
   <CommercialProjects />
+  <StaticWebsitesProjects />
 </template>
 
 <style scoped></style>
