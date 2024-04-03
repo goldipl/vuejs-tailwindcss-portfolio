3db6c0ec0af1335d9099a854767cc1061658d6f8
goldipl <mgodlewski92@gmail.com>
2024-04-03 14:30:34 +0200

    add a new component - Tapbar

--- src/components/common/HeaderComponent.vue
+++ src/components/common/HeaderComponent.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import TapbarComponent from '@/components/common/TapbarComponent.vue'
 </script>
 
 <template>
   <header
-    class="relative w-full m-auto before:content-[''] before:block before:bg-gradient-to-t from-purple-600 from-15% to-black before:top-0 before:left-0 before:absolute before:h-16 before:w-full mb-4"
+    class="flex items-center md:block relative w-full m-auto before:content-[''] before:hidden md:before:block before:bg-gradient-to-t from-purple-600 from-15% to-black before:top-0 before:left-0 before:absolute before:h-16 before:w-full mb-4"
   >
     <div
-      class="relative mx-auto max-w-[1200px] bg-gradient-to-t from-purple-600 from-15% to-black flex justify-between items-center h-16"
+      class="relative mx-auto max-w-[1200px] bg-gradient-to-t from-purple-600 from-15% to-black flex justify-center md:justify-between items-center h-14 md:h-16 w-full"
     >
       <RouterLink to="/">
-        <p class="relative text-white font-megrim text-4xl top-[-6px]">
+        <p class="relative text-white font-megrim text-3xl md:text-4xl top-[-6px]">
           Marcin Godlewski
-          <span class="absolute bottom-[-16px] right-[-24px] text-sm">Front-end Developer</span>
+          <span class="absolute bottom-[-11px] md:bottom-[-16px] right-0 text-xs md:text-sm"
+            >Front-end Developer</span
+          >
         </p>
       </RouterLink>
-      <nav class="flex justify-between items-center w-3/5">
+      <nav class="hidden md:flex justify-between items-center w-3/5">
         <RouterLink class="text-gray-300 text-lg hover:text-white" to="/">Home</RouterLink>
         <RouterLink class="text-gray-300 text-lg hover:text-white" to="/about-me"
           >O mnie</RouterLink
@@ -32,4 +35,5 @@ import { RouterLink } from 'vue-router'
       </nav>
     </div>
   </header>
+  <TapbarComponent />
 </template>
---
+++ src/components/common/TapbarComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <!-- Tapbar -->
+  <div
+    class="flex items-center justify-around fixed bottom-0 left-0 h-16 w-full bg-purple-600 z-50 md:hidden"
+  >
+    <RouterLink class="flex flex-col items-center justify-center text-white text-lg" to="/">
+      <img
+        src="./../../assets/img/tapbar/home.svg"
+        alt="Home"
+        class="text-xs mb-1 mt-1"
+        width="24"
+        height="24"
+      />
+      <span class="text-xs">Home</span>
+    </RouterLink>
+    <RouterLink class="flex flex-col items-center justify-center text-white text-lg" to="/about-me">
+      <img
+        src="./../../assets/img/tapbar/about-me.svg"
+        alt="O mnie"
+        class="text-xs mb-1 mt-1"
+        width="24"
+        height="24"
+      />
+      <span class="text-xs">O mnie</span>
+    </RouterLink>
+    <RouterLink
+      class="flex flex-col items-center justify-center text-white text-lg"
+      to="/portfolio"
+    >
+      <img
+        src="./../../assets/img/tapbar/portfolio.svg"
+        alt="O mnie"
+        class="text-xs mb-1 mt-1"
+        width="24"
+        height="24"
+      />
+      <span class="text-xs">Portfolio</span>
+    </RouterLink>
+    <RouterLink
+      class="flex flex-col items-center justify-center text-white text-lg"
+      to="/references"
+    >
+      <img
+        src="./../../assets/img/tapbar/references.svg"
+        alt="O mnie"
+        class="text-xs mb-1 mt-1"
+        width="24"
+        height="24"
+      />
+      <span class="text-xs">Referencje</span>
+    </RouterLink>
+    <RouterLink class="flex flex-col items-center justify-center text-white text-lg" to="/contact">
+      <img
+        src="./../../assets/img/tapbar/contact.svg"
+        alt="O mnie"
+        class="text-xs mb-1 mt-1"
+        width="24"
+        height="24"
+      />
+      <span class="text-xs">Kontakt</span>
+    </RouterLink>
+  </div>
+</template>
