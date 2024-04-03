5e81060ff2ae8a244aa2e50aa1302cb3849e45ba
goldipl <mgodlewski92@gmail.com>
2024-04-03 16:47:13 +0200

    correct path

--- src/components/buttons/SeeMyProjectsButton.vue
+++ src/components/buttons/SeeMyProjectsButton.vue
@@ -6,7 +6,7 @@ import { RouterLink } from 'vue-router'
   <div class="flex justify-center items-center my-4">
     <RouterLink
       class="text-gray-300 px-8 py-3 border border-purple-600 hover:text-purple-600 hover:bg-white transition-all duration-500"
-      to="/projects"
+      to="/portfolio"
       >Zobacz więcej moich projektów</RouterLink
     >
   </div>
