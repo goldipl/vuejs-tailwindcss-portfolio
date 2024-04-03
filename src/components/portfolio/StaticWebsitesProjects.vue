4c4df6ab0085b19402565ab214935ab43b794358
goldipl <mgodlewski92@gmail.com>
2024-04-03 15:59:14 +0200

    add old portfolio to my done projects

--- src/components/portfolio/StaticWebsitesProjects.vue
+++ src/components/portfolio/StaticWebsitesProjects.vue
@@ -301,7 +301,7 @@
         <div class="relative border border-1 border-purple-600">
           <img
             src="./../../assets/img/portfolio/static_websites/static_website_12.jpg"
-            alt="Jezenie Karoli, Bezglutenowa Karola"
+            alt="Jedzenie Karoli, Bezglutenowa Karola"
             width="410"
             height="205"
             class="h-auto"
@@ -318,6 +318,32 @@
             <a
               class="uppercase font-semibold text-white text-sm hover:text-black"
               href="https://github.com/goldipl/jedzenie-karoli"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/static_websites/static_website_13.jpg"
+            alt="Marcin Programuje"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/Marcin-Programuje/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/Marcin-Programuje"
               target="_blank"
               >code</a
             >
