b5ce593034e656098f9a5057fce9bb937be695f8
goldipl <mgodlewski92@gmail.com>
2024-04-03 11:50:50 +0200

    separate "murator projekty" website to a new component

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import ShortHeroComponent from '../components/portfolio/ShortHeroComponent.vue'
 import CommercialProjects from '../components/portfolio/CommercialProjects.vue'
+import MuratorProjectsWebsite from '../components/portfolio/MuratorProjectsWebsite.vue'
 import StaticWebsitesProjects from '../components/portfolio/StaticWebsitesProjects.vue'
 import ModernWebsitesProjects from '../components/portfolio/ModernWebsitesProjects.vue'
 import PromobisWebsite from '../components/portfolio/PromobisWebsite.vue'
@@ -12,6 +13,7 @@ import PromotecaWebsite from '../components/portfolio/PromotecaWebsite.vue'
 <template>
   <ShortHeroComponent />
   <CommercialProjects />
+  <MuratorProjectsWebsite />
   <StaticWebsitesProjects />
   <ModernWebsitesProjects />
   <PromobisWebsite />
