cb9770c848b1a1708c1ad68a3bfdbee77d1d5be4
goldipl <mgodlewski92@gmail.com>
2024-04-03 13:44:34 +0200

    add references to subpage

---
+++ src/components/references/ReferencesSection.vue
@@ -0,0 +1,88 @@
+<template>
+  <section class="my-20">
+    <div class="projects-container mb-24">
+      <div class="wide-wrapper flex flex-wrap justify-center items-center gap-4 my-6">
+        <!-- Reference -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/references/reference01.jpg"
+            alt="Reference"
+            width="400"
+            height="800"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/vuejs-tailwindcss-portfolio/assets/img/references/reference01.jpg"
+              target="_blank"
+              >powiększ</a
+            >
+          </div>
+        </div>
+        <!-- Reference -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/references/reference02.jpg"
+            alt="Reference"
+            width="400"
+            height="800"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/vuejs-tailwindcss-portfolio/assets/img/references/reference02.jpg"
+              target="_blank"
+              >powiększ</a
+            >
+          </div>
+        </div>
+        <!-- Reference -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/references/reference03.jpg"
+            alt="Reference"
+            width="400"
+            height="800"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/vuejs-tailwindcss-portfolio/assets/img/references/reference03.jpg"
+              target="_blank"
+              >powiększ</a
+            >
+          </div>
+        </div>
+        <!-- Reference -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/references/reference04.jpg"
+            alt="Reference"
+            width="400"
+            height="800"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/vuejs-tailwindcss-portfolio/assets/img/references/reference04.jpg"
+              target="_blank"
+              >powiększ</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/views/ReferencesView.vue
+++ src/views/ReferencesView.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import ShortHeroComponent from '../components/references/ShortHeroComponent.vue'
+import ShortHeroComponent from '@/components/references/ShortHeroComponent.vue'
+import ReferencesSection from '@/components/references/ReferencesSection.vue'
 </script>
 
 <template>
   <ShortHeroComponent />
-  <div class="wrapper references-page">
-    <h1>This is a references page</h1>
-  </div>
+  <ReferencesSection />
 </template>
-
-<style scoped></style>
