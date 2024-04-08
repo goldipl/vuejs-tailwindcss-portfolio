55531a2f1deb82a89245a06720748cdb901fe044
goldipl <mgodlewski92@gmail.com>
2024-04-08 15:52:37 +0200

    add Vite to tools section

--- src/components/home/ToolsSection.vue
+++ src/components/home/ToolsSection.vue
@@ -132,6 +132,13 @@
         width="80"
         height="80"
       />
+      <img
+        class="m-4 w-16 h-16 md:w-20 md:h-20"
+        src="./../../assets/img/home/tools/vite.svg"
+        alt="Vite"
+        width="80"
+        height="80"
+      />
     </div>
   </section>
 </template>
