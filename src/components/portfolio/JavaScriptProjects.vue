f4f5264eb54edddacb9984a5db4de41e38d08d15
goldipl <mgodlewski92@gmail.com>
2024-04-29 17:06:03 +0200

    add a new react project

--- src/components/portfolio/JavaScriptProjects.vue
+++ src/components/portfolio/JavaScriptProjects.vue
@@ -361,6 +361,32 @@
             >
           </div>
         </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/js/JS15.jpg"
+            alt="JavaScript Project"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://goldipl.github.io/react-cms-component/"
+              target="_blank"
+              >live</a
+            >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://github.com/goldipl/react-cms-component"
+              target="_blank"
+              >code</a
+            >
+          </div>
+        </div>
       </div>
     </div>
   </section>
