f52b4b6626d2122b04fa8fbe8e785e2ce6976322
goldipl <mgodlewski92@gmail.com>
2024-06-05 10:53:39 +0200

    add a new commercial project

--- src/components/portfolio/CommercialProjects.vue
+++ src/components/portfolio/CommercialProjects.vue
@@ -350,6 +350,26 @@
             >
           </div>
         </div>
+        <!-- Project -->
+        <div class="relative border border-1 border-purple-600">
+          <img
+            src="./../../assets/img/portfolio/commercial_zpr/CommercialWebsite18.jpg"
+            alt="targivivelo.pl"
+            width="410"
+            height="205"
+            class="h-auto"
+          />
+          <div
+            class="absolute left-0 bottom-0 bg-gradient-to-r from-purple-600 from-45% to-black w-48 p-2 flex justify-around items-center"
+          >
+            <a
+              class="uppercase font-semibold text-white text-sm hover:text-black"
+              href="https://www.poradyplus.pl/"
+              target="_blank"
+              >live</a
+            >
+          </div>
+        </div>
       </div>
     </div>
   </section>
